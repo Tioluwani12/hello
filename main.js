a6a82a4eaa3fc8e7fc644dcd1558739274e4823f
Tioluwani12 <95766690+Tioluwani12@users.noreply.github.com>
2022-03-08 16:47:28 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabrick.Image.fromURL('BirthdayImage.jpg', function(Img)	
+	
+}
+
+function playSound(){
+	x.play();
+}
